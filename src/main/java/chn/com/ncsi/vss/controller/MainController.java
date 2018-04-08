d9deff3708c50111b8bccbfc3ab3c443046ce40f
LebronKingJames23 <huangyingjie921020@163.com>
2018-04-08 18:51:04 +0800

    test2

--- src/main/java/chn/com/ncsi/vss/controller/MainController.java
+++ src/main/java/chn/com/ncsi/vss/controller/MainController.java
@@ -41,81 +41,83 @@ public String toWelcome() {
 public String toUploadPage(){
 		return "upload";
 	}
-@RequestMapping("uploadFile")  
-public  String  upload(HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{ 
-		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request; 
-		MultipartFile file = multipartRequest.getFile("upfile");   
-		if(file.isEmpty()){  
-	            throw new Exception("empty");  
-	        }  
+	@RequestMapping("uploadFile")
+	public  String  upload(HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		MultipartFile file = multipartRequest.getFile("upfile");
+		if(file.isEmpty()){
+			throw new Exception("empty");
+		}
 		String fileName = file.getOriginalFilename().toString();
+		System.out.println(fileName);
+		System.out.println(file.getName());
 		List<String[]> list = new ArrayList<String[]>();
-		InputStream is = file.getInputStream();// 瑙ｆ瀽鐨勬槸涓婁紶鐨剎lsx鏂囦欢
-		
-		// 鍒涘缓璇诲彇瀵硅薄
-		Workbook workbook = WorkbookFactory.create(is);  
-		Sheet sheet = workbook.getSheetAt(0);  //绀烘剰璁块棶sheet  
-		// 鑾峰彇琛ㄩ噷闈㈡湁澶氬皯琛�
+		// 准备要读取的文件流
+		InputStream is = file.getInputStream();// 解析的是上传的xlsx文件
+		// 创建读取对象
+		Workbook workbook = WorkbookFactory.create(is);
+		Sheet sheet = workbook.getSheetAt(0);  //示意访问sheet
+		// 获取表里面有多少行
 		int num = sheet.getLastRowNum();
-		// 閬嶅巻琛�
+		// 遍历行
 		boolean flag=true;
 		for (int i = 0; i < num; i++) {
-			// 鑾峰彇琛屽璞�
-			Row row = sheet.getRow(i + 1);// 鎸夌収鎴戜滑鐨勮瀹氫笂浼犵殑xlsx鏂囦欢鐨勫寘鍚〃澶寸殑锛屾帓闄よ〃澶磇 = 1
+			// 获取行对象
+			Row row = sheet.getRow(i + 1);// 按照我们的规定上传的xlsx文件的包含表头的，排除表头i = 1
 			if(row==null){
-			break;
+				break;
 			}
 			short cellNum = row.getLastCellNum();
-			// 瀹炰緥鍖栨暟缁�
+			// 实例化数组
 			String[] strings = new String[cellNum];
 			for (int j = 0; j < cellNum; j++) {
-				// 鑾峰彇鏍煎瓙瀵硅薄
+				// 获取格子对象
 				Cell cell = row.getCell(j);
-				// 鑾锋牸鏍煎瓙瀵硅薄閲岄潰鐨勬暟鎹�
-				   cell.setCellType(Cell.CELL_TYPE_STRING);
+				// 获格格子对象里面的数据
+				cell.setCellType(Cell.CELL_TYPE_STRING);
 				strings[j] = cell.getStringCellValue();
 			}
 			;
 			for(int k=0;k<strings.length;k++) {
-				if(strings[0].equals("杩涘叆NCS CD鍔炲叕鍖哄煙锛岃閬靛畧鏂扮數淇℃伅绉戞妧锛堟垚閮斤級鏈夐檺鍏徃鐨勪俊鎭畨鍏ㄧ鐞嗚姹傘�")){
+				if(strings[0].equals("进入NCS CD办公区域，请遵守新电信息科技（成都）有限公司的信息安全管理要求。")){
 					flag=false;
 				}
-				}
-			// 娣诲姞鏁版嵁鍒伴泦鍚�
-				if(flag==true){	
-			list.add(strings);
-				}
 			}
+			// 添加数据到集合
+			if(flag==true){
+				list.add(strings);
+			}
+		}
 		List list2 = new ArrayList();
 		List list3=new ArrayList();
-	   for (String[] strings : list) {
-	        	Visitor visitor = new Visitor();
-				visitor.setMOBILE(strings[3]);
-				visitor.setSTATUS("P");
-				list2.add(strings[2]);
-				visitor=visitorService.findVisitorByTel(visitor);
-				if(visitor!=null){
-					continue;
-				}
-				Visitor visitor1 = new Visitor();
-				list3.add(strings[2]);
-				visitor1.setMOBILE(strings[3]);
-				visitor1.setSTATUS("P");
-				visitor1.setNAME(strings[2]);
-				visitor1.setPURPOSE_CODE("I");
-				visitor1.setJOB_POSITION(strings[6]);
-				visitor1.setSTAFF_NAME(strings[9]);
-				visitor1.setCREATED_ID("ORP");
-				visitor1.setCREATED_DATE(new Timestamp(new Date().getTime()));
-				visitorService.addInterviewr(visitor1);
-	   }
-	  model.addAttribute("totalList", list2);
-	  model.addAttribute("importList", list3);
-	  model.addAttribute("totalCount",list2.size());
-	  model.addAttribute("importCount", list3.size());
-        return "uploadSuccess";
-    }  
-@RequestMapping("tologinPage")
+		for (String[] strings : list) {
+			Visitor visitor = new Visitor();
+			visitor.setMOBILE(strings[3]);
+			visitor.setSTATUS("P");
+			list2.add(strings[2]);
+			visitor=visitorService.findVisitorByTel(visitor);
+			if(visitor!=null){
+				continue;
+			}
+			Visitor visitor1 = new Visitor();
+			list3.add(strings[2]);
+			visitor1.setMOBILE(strings[3]);
+			visitor1.setSTATUS("P");
+			visitor1.setNAME(strings[2]);
+			visitor1.setPURPOSE_CODE("I");
+			visitor1.setJOB_POSITION(strings[6]);
+			visitor1.setSTAFF_NAME(strings[9]);
+			visitor1.setCREATED_ID("ORP");
+			visitor1.setCREATED_DATE(new Timestamp(new Date().getTime()));
+			visitorService.addInterviewr(visitor1);
+		}
+		model.addAttribute("totalList", list2);
+		model.addAttribute("importList", list3);
+		model.addAttribute("totalCount",list2.size());
+		model.addAttribute("importCount", list3.size());
+		return "uploadSuccess";
+	}
+	@RequestMapping("tologinPage")
 public String toLoginPage() {
 return "login";
 }
